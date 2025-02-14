f6da014cb25830d1ec3d3698e66b5078a4e13ca8
ISCLuisRamirez <isc.luisramirez@gmail.com>
2025-02-14 12:58:29 -0600

    #9 - via_detail field added to requets

--- Models/Request.cs
+++ Models/Request.cs
@@ -14,13 +14,14 @@ public class Request
     public int? id_via { get; set; }
     [StringLength(100, ErrorMessage = "El nombre de sububicación no puede exceder los 100 caracteres.")]
     public string? name_sublocation { get; set; }
-    public DateOnly? date { get; set; }
-    public DateOnly created_date { get; set; }
-    public DateOnly? updated_date { get; set; }
-    public DateOnly? closed_date { get; set; }
+    public string? via_detail { get; set; }
     public string? period { get; set; }
     public string? folio { get; set; } = string.Empty;
     public string? password { get; set; } = string.Empty;
     public string? description { get; set; } = string.Empty;
     public int status { get; set; }
+    public DateOnly? date { get; set; }
+    public DateOnly created_date { get; set; }
+    public DateOnly? updated_date { get; set; }
+    public DateOnly? closed_date { get; set; }
 }
