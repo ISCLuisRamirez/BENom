574d5655311c45df01addbe7ab3ab296caec12fc
ISCLuisRamirez <isc.luisramirez@gmail.com>
2025-02-27 15:25:40 -0600

    department field added

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@ public class User
 {
     public int id { get; set; }
     public int id_role { get; set; }
+    public int id_department {get; set;}
     [StringLength(6, ErrorMessage = "El numero de empleado no puede exceder los 6 caracteres.")]
     public string employee_number { get; set; }
     public string email { get; set; }
--- obj/Debug/net9.0/BENom.AssemblyInfo.cs
+++ obj/Debug/net9.0/BENom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BENom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b09fc01a56a5328ad73f74115a5356dfd7d8ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23406592e7e7d577c21410682c30ba92a2a19b8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BENom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BENom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
